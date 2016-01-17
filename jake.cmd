54c074354f6408b1e06966e6ed72fed315d46ce1
cuspace <cuspace@gmail.com>
2016-01-17 23:33:02 +0900

    Build script checks Node version

--- jake.cmd
+++ jake.cmd
@@ -1,2 +1,4 @@
 @echo off
+echo Building npm modules:
+npm rebuild
 node_modules\.bin\jake %*
--- jakefile.js
+++ jakefile.js
@@ -2,7 +2,18 @@
     "use strict";
 
     desc("Default build");
-    task("default", function() {
+    task("default", ["version"], function() {
         console.log("\n\nBUILD OK");
-    ;});
+    });
+
+    var EXPECTED_NODE_VERSION = "v5.4.0";
+    desc("Check Node version");
+    task("version", function() {
+        console.log("Checking Node version: .");
+        var actualVersion = process.version;
+        if (actualVersion !== EXPECTED_NODE_VERSION) {
+            fail("Incorrect Node version: expected " +
+                 EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+        }
+    });
 }());
